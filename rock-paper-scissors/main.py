a85561edbeae2c76a133cfdaa3caf3910ffd32f8
Rick Verbon <rick89@gmail.com>
2022-09-18 11:44:47 +0200

    removed whitespace

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -1,13 +1,11 @@
 import random
 
 exit = False
-
 choices = ["Rock", "Paper", "Scissors"]
 
 while not exit:
     opponentChoice = random.choice(choices)
     playerChoice = input("Rock, paper, scissors?: ")
-
     if(playerChoice == opponentChoice):
         print("It's a draw")
         print("")
@@ -19,6 +17,3 @@ while not exit:
     else:
         print(f"{opponentChoice} beats {playerChoice}, computer won.")
         print("")
-        
-
-
