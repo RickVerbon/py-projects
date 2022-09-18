a8725e841ae00c3f99b8b1f2b65e9402a74b0fba
Rick Verbon <rick89@gmail.com>
2022-09-18 12:29:33 +0200

    Added logic so only numbers can be entered

--- guess-number/main.py
+++ guess-number/main.py
@@ -8,15 +8,22 @@ def getRandomNumber():
 rndNum = getRandomNumber()
 
 while not exit:
-    choice = int(input("Enter a number between 0 and 100, enter '-1'  to quit: "))
-    if(choice < rndNum):
-        print("Higher")
-    elif(choice > rndNum):
-        print("Lower")
-    elif(choice == rndNum):
-        print("You guessed the number right.")
-        rndNum = getRandomNumber()
-    elif(choice == -1):
-        exit = True
-    else:
-        print("Wrong input")
+    choice = input("Enter a number between 0 and 100, enter 'x'  to quit: ")
+    if(choice.isnumeric()):
+        if(int(choice) >= 0 or int(choice) <= 100):
+            if(int(choice) < rndNum):
+                print("Higher")
+            elif(int(choice) > rndNum):
+                print("Lower")
+            elif(int(choice) == rndNum):
+                print("You guessed the number right.")
+                print("______________________________")
+                print("Generated new number")
+                rndNum = getRandomNumber()
+            else:
+                continue
+    elif(choice == 'x'):
+        exit == True
+    else: 
+        print("Invalid input, please enter a number to play, or 'x' to quit")
+        
