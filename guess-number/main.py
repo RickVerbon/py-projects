21efe060d8a67886a534d5a244d96cf8b8d2a618
Rick Verbon <rick89@gmail.com>
2022-09-18 12:10:33 +0200

    added some if statements

--- guess-number/main.py
+++ guess-number/main.py
@@ -5,13 +5,18 @@ exit = False
 def getRandomNumber():
     return random.randrange(1, 100);
 
-rndNum = getRandomNumber();
+rndNum = getRandomNumber()
 
 while not exit:
-    choice = int(input("Enter a number between 0 and 100: "))
+    choice = int(input("Enter a number between 0 and 100, enter '-1'  to quit: "))
     if(choice < rndNum):
         print("Higher")
     elif(choice > rndNum):
         print("Lower")
-    if(choice == rndNum):
+    elif(choice == rndNum):
         print("You guessed the number right.")
+        rndNum = getRandomNumber()
+    elif(choice == -1):
+        exit = True
+    else:
+        print("Wrong input")
